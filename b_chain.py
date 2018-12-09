718d7763427354791dfc71b5282f130565d0ff5b
tonygm2 <tonygms2@gmail.com>
2018-12-09 23:43:35 +0600

    added new function

--- b_chain.py
+++ b_chain.py
@@ -8,4 +8,25 @@ class Block:
 	#5 A nonce is a number only used once
 	#6 store the hash (ID) of the previous block in the chain
 	#7 timestamp
+	blockNo = 0
+	data = None
+	next = None
+	hash = None
+	nonce = 0
+	previous_hash = 0x0
+	timestamp = datetime.datetime.now()
 	
+	def __init__(self,data):
+		self.data = data
+	
+	#function to calculate 'hash' of a block
+	def hash(self):
+		h = hashlib.sha256()
+		h.update(
+		str(self.nonce).encode('utf-8') +
+		str(self.data).encode('utf-8') +
+		str(self.previous_hash).encode('utf-8') +
+		str(self.timestamp).encode('utf-8') +
+		str(self.blockNo).encode('utf-8')
+		
+		)
